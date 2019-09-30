a2af3102f4dbf56a82aca2e4086edd21dffce619
sigmarion1 <axsim@naver.com>
2019-10-01 02:20:10 +0900

    first commit

---
+++ photo/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Photo
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'created', 'updated']
+    raw_id_fields = ['author']
+    list_filter = ['created', 'updated', 'author']
+    search_fields = ['text', 'created']
+    ordering = ['-updated', '-created']
+
+
+admin.site.register(Photo, PhotoAdmin)
---
+++ photo/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.shortcuts import redirect
+
+from .models import Photo
+
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', {'photos': photos})
+
+
+class PhotoUploadView(CreateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/upload.html'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form': form})
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
+
+class PhotoUpdateView(UpdateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/update.html'
