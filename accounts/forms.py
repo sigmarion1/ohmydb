e1f4ee941555567d9d0e1d27e72891798526cf6b
sigmarion1 <axsim@naver.com>
2019-10-08 19:21:24 +0900

    email deleted

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ class RegisterForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username']
 
     def clean_password2(self):
         cd = self.cleaned_data
