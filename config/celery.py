611e81c4f7e452880c9fe21bdd57ef85736d024d
sigmarion1 <axsim@naver.com>
2019-11-05 02:00:07 +0900

    celery schedule applied

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedule import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -20,3 +21,27 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+'''
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'crawling.tasks.print_log',
+        'schedule': 1.0,
+        'args': None,
+    },
+}
+
+'''
+app.conf.beat_schedule = {
+    'crawling': {
+        'task': 'crawling.tasks.crawl_task',
+        'schedule': crontab(minute='*/43', hour='*/3,8-17')
+        'args': None,
+    },
+
+    'listing': {
+        'task': 'crawling.tasks.list_omg_task',
+        'schedule': crontab(minute-'*/55', hour='10-15')
+        'args': None,
+    },
+}
--- crawling/tasks.py
+++ crawling/tasks.py
@@ -2,21 +2,12 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from .crawling import Crawler
+import random
 
 
 @shared_task
 def add(x, y):
     return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
 
 @shared_task
 def print_log(log=None):
@@ -31,3 +22,10 @@ def print_log(log=None):
 def crawl_task(asc):
     c = Crawler()
     return(c.save_a_photo_from_list())
+
+@shared_task
+def list_omg_task(asc):
+    c = Crawler()
+    board = "ohmygirl"
+    page = random.randrange(1,10)
+    return(c.get_recommend_post(board,page))
