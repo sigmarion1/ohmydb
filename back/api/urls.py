00fcae907e7cb2cbf4ec1f93abef280c88845883
sigmarion1 <axsim@naver.com>
2023-09-22 18:53:45 +0900

    fix trailing slash issue

--- back/api/urls.py
+++ back/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import ImageModelViewset
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 
 router.register(r"images", ImageModelViewset)
 
