2e4b6f0e6e065f372c3d1e87453d05377727c886
sigmarion1 <axsim@naver.com>
2023-09-23 09:58:42 +0900

    init face app

--- back/face/app.py
+++ back/face/app.py
@@ -1,5 +1,12 @@
 import os
 import django
 
+
+from api.models import Image
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ohmydb.settings")
 django.setup()
+
+a = Image.objects.all()
+print(a)
